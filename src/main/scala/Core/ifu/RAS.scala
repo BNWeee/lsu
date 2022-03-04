6d66ac0ebf297f6e52c38bcc60b3caf6067561ab
bigzyb <lightningreturns@163.com>
2022-03-04 10:51:47 +0800

    ras riscv1 add

--- src/main/scala/Core/ifu/RAS.scala
+++ src/main/scala/Core/ifu/RAS.scala
@@ -13,11 +13,16 @@ class RASIO extends CoreBundle {
   val ibdp_ras_push_pc        = Input(UInt(VAddrBits.W))
   val ibctrl_ras_pcall_vld    = Input(Bool())
   val ibctrl_ras_preturn_vld  = Input(Bool())
+  val rtu_retire0_pc        = Input(UInt(VAddrBits.W))
+  val ras_en                = Input(Bool())
+  val pcall                 = Input(Bool())   //from ip call mask
+  val preturn               = Input(Bool())
 
-  val ras_ipdp_pc             = Output(UInt(VAddrBits.W))
+  val ras_data_vld            = Output(Bool())
   val ras_target_pc           = Output(UInt(VAddrBits.W))
-  val ras_l0_btb_pc           = Output(UInt(VAddrBits.W))
-  val ras_l0_btb_push_pc      = Output(UInt(VAddrBits.W))
+  val ras_ubtb_pc           = Output(UInt(VAddrBits.W))
+  val ras_ubtb_push_pc      = Output(UInt(VAddrBits.W))
+  val ras_ubtb_push         = Output(Bool())
 }
 class RAS extends Module with Config {
   val io = IO(new RASIO)
@@ -29,8 +34,8 @@ class RAS extends Module with Config {
   val ras_push_pc = Wire(UInt(VAddrBits.W))
   val ras_pc_out = Wire(UInt(VAddrBits.W))
   ras_push := io.ibctrl_ras_pcall_vld
-  ras_push := io.ibctrl_ras_preturn_vld
-  ras_push_pc(VAddrBits-1,0) := io.ibdp_ras_push_pc(VAddrBits-1,0)
+  ras_pop  := io.ibctrl_ras_preturn_vld
+
 
   //rtu fifo
   val rtu_ifu_pcall = Wire(Bool())
@@ -39,12 +44,14 @@ class RAS extends Module with Config {
   rtu_ifu_preturn := io.rtu_ifu_preturn
 
   val rtu_ptr     = RegInit(0.U(5.W))
-  val rtu_ptr_pre = RegInit(0.U(5.W))
+  val rtu_ptr_pre = Wire(0.U(5.W))
   val ras_ptr     = RegInit(0.U(5.W))
-  val ras_ptr_pre = RegInit(0.U(5.W))
+  val ras_ptr_pre = Wire(0.U(5.W))
   val status_ptr  = RegInit(0.U(5.W))
+  val status_ptr_pre = Wire(0.U(5.W))
 
 
+  //rtu ptr
   val rtu_ras_empty = (rtu_ptr === status_ptr)
   when(rtu_ifu_preturn && rtu_ifu_pcall) {
     rtu_ptr_pre := rtu_ptr
@@ -63,12 +70,11 @@ class RAS extends Module with Config {
   }.otherwise {
     rtu_ptr_pre := rtu_ptr
   }
-  rtu_ptr := Mux(io.cp0_ifu_ras_en,rtu_ptr_pre,rtu_ptr)
+  rtu_ptr := Mux(io.ras_en,rtu_ptr_pre,rtu_ptr)
 
   //ras ptr
   val rtu_need = io.rtu_ifu_mispred || io.rtu_ifu_flush
-  val ras_empty = ras_ptr === status_ptr
-  val ras_full  = ras_ptr === Cat(!status_ptr(4),status_ptr(3,0))
+
   when(rtu_need) {
     ras_ptr_pre := rtu_ptr_pre
   }.elsewhen(ras_push && ras_return) {
@@ -88,15 +94,73 @@ class RAS extends Module with Config {
   }.otherwise {
     ras_ptr_pre := ras_ptr
   }
-  ras_ptr := Mux(io.cp0_ifu_ras_en,ras_ptr_pre,ras_ptr)
+  ras_ptr := Mux(io.ras_en,ras_ptr_pre,ras_ptr)
 
-  ras_pc_out            := ras.read(rtu_ptr)
-  when(ras_push){
-    ras.write(rtu_ptr,ras_push_pc)
+  //status ptr  used to solve ras overflow
+  status_ptr_pre := Mux(status_ptr(3,0)==="b1011".U,Cat(status_ptr(4),"b0000".U),status_ptr+1.U)
+  when(io.ras_en && rtu_need) {
+    when(rtu_ptr_pre(4) ^ ras_ptr(4)) {
+      status_ptr := 0.U
+    }.otherwise {
+      status_ptr := Cat(status_ptr(4), rtu_ptr_pre(3,0))
+    }
+  }.elsewhen(io.ras_en && ras_full && ras_push && ras_pop) {
+    status_ptr := status_ptr
+  }.elsewhen(io.ras_en && ras_full && ras_push) {
+    status_ptr := status_ptr_pre
+  }.elsewhen(io.ras_en && ras_full && ras_pop) {
+    when(status_ptr(3,0) === 0.U) {
+      status_ptr := 0.U
+    }.otherwise {
+      status_ptr := status_ptr - 1.U
+    }
+  }.otherwise {
+    status_ptr := status_ptr
   }
-  io.ras_ipdp_pc        := Mux(io.ibctrl_ras_inst_pcall,io.ibdp_ras_push_pc,ras_pc_out)
-  io.ras_target_pc      := Mux(io.ibctrl_ras_inst_pcall,io.ibdp_ras_push_pc,ras_pc_out)
-  io.ras_l0_btb_push_pc := io.ibdp_ras_push_pc
-  io.ras_ipdp_pc        := ras_pc_out
 
+  val ras_empty = ras_ptr === status_ptr
+  val ras_full  = ras_ptr === Cat(!status_ptr(4),status_ptr(3,0))
+
+  val rtu_fifo_ptr  = RegInit(0.U(4.W))
+  val rtu_fifo_ptr_pre = Wire(0.U(4.W))
+  rtu_fifo_ptr_pre := Mux(io.rtu_ifu_pcall,rtu_ptr(3,0),rtu_fifo_ptr(3,0))
+  when(io.ras_en && io.rtu_ifu_pcall) {
+    rtu_fifo_ptr := rtu_fifo_ptr_pre
+  }
+
+
+  //rtu fifo
+  val rtu_push_index = Wire(0.U(6.W))
+  for(i <- 0 until 6) {
+    when(rtu_ptr(3,0) === (0+i).U || rtu_ptr(3,0) === (6+i).U) {
+      rtu_push_index := i.U
+    }
+  }
+
+  // ras push
+  val pushindex = ras_ptr(3,0)
+  val rtu_index = Wire(0.U(4.W))
+  for(i <- 0 until 12) {
+    when(rtu_fifo_ptr_pre === (0+i).U
+    ||rtu_fifo_ptr_pre === (0+i).U(3,0) ||  rtu_fifo_ptr_pre === (1+i).U(3,0) ||  rtu_fifo_ptr_pre === (2+i).U(3,0)
+      ||rtu_fifo_ptr_pre === (3+i).U(3,0) ||  rtu_fifo_ptr_pre === (4+i).U(3,0) ||  rtu_fifo_ptr_pre === (5+i).U(3,0)) {
+      rtu_index := i.U
+    }
+  }
+  val rtu_copy_index = rtu_index
+  val ras_filled = rtu_need && (rtu_copy_index===ras_ptr(3,0))
+
+  val ras_index = Mux(rtu_need,rtu_copy_index,ras_ptr(3,0))
+
+  ras_push_pc := Mux(rtu_need,io.rtu_retire0_pc,io.ibdp_ras_push_pc)
+
+  ras_pc_out            := ras.read(ras_index)
+  when(ras_push || rtu_need){
+    ras.write(ras_index,ras_push_pc)
+  }
+  io.ras_data_vld       := !ras_empty || ras_push && ras_filled
+  io.ras_target_pc      := Mux(io.pcall,io.ibdp_ras_push_pc,ras_pc_out)
+  io.ras_ubtb_pc        := ras_pc_out
+  io.ras_ubtb_push_pc   := io.ibdp_ras_push_pc
+  io.ras_ubtb_push      := ras_push && io.ras_en
 }
